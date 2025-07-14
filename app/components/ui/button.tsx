eab655de924b8b291d3e9350b94cb844ebb046cf
debrise <Steven@Thehernandezco.com>
2025-07-14 10:17:58 -0700

    m

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -31,7 +31,7 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'disabled'>,
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
   isLoading?: boolean;
